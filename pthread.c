6a220828409136604d277547733b6727b3b29121
devleomk1 <wltnskjs@naver.com>
2021-09-16 16:23:46 +0900

    add

--- pthread.c
+++ pthread.c
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 15:01:34 by jisokang          #+#    #+#             */
-/*   Updated: 2021/09/16 12:49:29 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/09/16 14:33:53 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-// thread_void : 스레드로 호출할 함수 파라미터를 void 포인터로 안주면 warning이 뜬다.
+// void *thread_void : pthread_create()함수의 스레드로 분기할 함수는 void* 타입만 사용가능하고,
+// 매개변수도 void* 이어야만 사용 가능하다. ! 중요 !
 static void	*routine(void *thread_void)
 {
 	int	*p;
 
 	p = (void *)thread_void;
 	printf("Hello Thread\n");
-	usleep(2000000);
+	usleep(200000);
 	printf("End thread\n");
 	return (0);
 }
@@ -34,7 +35,13 @@ int	main(void)
 
 	pthread_create(&t1, NULL, &routine, NULL);
 	pthread_create(&t2, NULL, &routine, NULL);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
+	//pthread를 생성하는 순간 main문과는 별도로 routine()함수가 돌아간다.
+	usleep(1000000);
+	// pthread_join(t1, NULL);
+	// pthread_join(t2, NULL);
+	pthread_detach(t1);
+	pthread_detach(t2);
+
+	printf("THE END\n");
 	return (0);
 }
